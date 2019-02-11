580fe16490a06d1c40ee7656b770cd778a03dc2f
Christopher Kalfas <chriskalfas@yahoo.com>
2019-02-11 19:08:58 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ end
    board.all?{|i| i == "X" || i == "O"}
  end
  
- def draw(board)
+ def draw?(board)
    if !won?(board) && full?(board)
      return true
    elsif !won?(board) && !full?(board)
